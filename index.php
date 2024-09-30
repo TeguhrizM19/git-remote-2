bb2649695244bd2f644255a087f96a916d5b6c26
TeguhrizM19 <teguhrizqim19@gmail.com>
2024-09-30 15:16:53 +0700

    menambahkan file index.php

--- index.php
+++ index.php
@@ -8,5 +8,7 @@
 <body>
   <h1>Hello Word!</h1>
   <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate dicta magnam quae deleniti recusandae a iure vero doloribus, molestiae amet dolorum distinctio qui odio itaque harum. Veritatis sint cumque deserunt!</p>
+  
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam deserunt assumenda, facere deleniti culpa, alias cum veniam vero repellendus, ipsam iure laudantium dolores atque et? Unde possimus deleniti repellat! Excepturi?</p>
 </body>
 </html>
